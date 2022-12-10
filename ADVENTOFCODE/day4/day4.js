cbb5c098553eab4d40a1e4023a9fee4c28501ecc
Mark Westerweel <mark.westerweel@hva.nl>
2022-12-10 22:29:25 +0100

    needed to parse. Day4 part 1

---
+++ ADVENTOFCODE/day4/day4.js
@@ -0,0 +1,38 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var rd = require("readline");
+var fileName = './day4.txt';
+var reader = rd.createInterface(fs.createReadStream(fileName));
+function getOverlap() {
+    var overlapCounter = 0;
+    reader.on('line', function (l) {
+        var pairs = getPairs(l);
+        var firstPairStartAndEnd = getPairStartAndEnd(pairs, 0);
+        var secondPairStartAndEnd = getPairStartAndEnd(pairs, 1);
+        var firstPairStart = getPairStart(firstPairStartAndEnd);
+        var firstPairEnd = getPairEnd(firstPairStartAndEnd);
+        var secondPairStart = getPairStart(secondPairStartAndEnd);
+        var secondPairEnd = getPairEnd(secondPairStartAndEnd);
+        if (firstPairStart <= secondPairStart && firstPairEnd >= secondPairEnd ||
+            secondPairStart <= firstPairStart && secondPairEnd >= firstPairEnd) {
+            overlapCounter += 1;
+        }
+        console.log(overlapCounter);
+    });
+}
+getOverlap();
+function getPairs(string) {
+    var pairs = string.split(',');
+    return pairs;
+}
+function getPairStartAndEnd(arr, index) {
+    var startAndEnd = arr[index].split('-');
+    return startAndEnd;
+}
+function getPairStart(arr) {
+    return parseInt(arr[0]);
+}
+function getPairEnd(arr) {
+    return parseInt(arr[1]);
+}
---
+++ ADVENTOFCODE/day4/day4.ts
@@ -0,0 +1,61 @@
+import * as fs from 'fs';
+import * as rd from 'readline'
+
+const fileName = './day4.txt';
+
+const reader = rd.createInterface(fs.createReadStream(fileName))
+
+function getOverlap(){
+
+    let overlapCounter = 0;
+    reader.on('line', (l:string) => {
+
+        let pairs  = getPairs(l);
+        let firstPairStartAndEnd = getPairStartAndEnd(pairs, 0)
+        let secondPairStartAndEnd = getPairStartAndEnd(pairs, 1)
+        let firstPairStart = getPairStart(firstPairStartAndEnd)
+        let firstPairEnd = getPairEnd(firstPairStartAndEnd)
+        let secondPairStart = getPairStart(secondPairStartAndEnd)
+        let secondPairEnd = getPairEnd(secondPairStartAndEnd)
+
+
+
+        if(
+            firstPairStart <= secondPairStart && firstPairEnd >= secondPairEnd || 
+            secondPairStart <= firstPairStart && secondPairEnd >= firstPairEnd) {
+                overlapCounter += 1
+            }
+        
+
+        console.log(overlapCounter);
+        
+    })
+
+
+
+}
+
+getOverlap();
+
+function getPairs(string: string){
+    let pairs = string.split(',')
+  
+    
+    return pairs
+}
+
+function getPairStartAndEnd(arr: string[], index: number){
+    let startAndEnd = arr[index].split('-')
+   
+    
+    return startAndEnd;
+}
+
+function getPairStart(arr: string[]){
+   
+    return parseInt(arr[0]);
+}
+
+function getPairEnd(arr:string[]){
+    return parseInt(arr[1])
+}
